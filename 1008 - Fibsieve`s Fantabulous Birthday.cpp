d9b729d8d548193dc7f75ae6f27d18011a3091d3
Ahanabaiub <54981138+Ahanabaiub@users.noreply.github.com>
2020-06-10 17:00:45 +0600

    Update 1008 - Fibsieve`s Fantabulous Birthday.cpp

--- 1008 - Fibsieve`s Fantabulous Birthday.cpp
+++ 1008 - Fibsieve`s Fantabulous Birthday.cpp
@@ -1,44 +1,80 @@
 #include<bits/stdc++.h>
-#define fios ios_base::sync_with_stdio(false); cin.tie(0);
-#define sf(x) scanf("%d",&x)
-#define sfl(x) scanf("%lld",&x)
-#define pf(x) printf("%d\n",x)
-#define pfl(x) printf("%lld\n",x)
-#define ll long long
-#define nl "\n"
+#define    fios ios_base::sync_with_stdio(false); cin.tie(0);
+#define    sf(x) scanf("%d",&x)
+#define    sfl(x) scanf("%lld",&x)
+#define    pf(x) printf("%d\n",x)
+#define    pfl(x) printf("%lld\n",x)
+#define    ll long long
+#define    nl "\n"
+#define    PB(x) push_back(x)
+#define    For(i,n) for(int i=0; i<n; i++)
+#define    PI acos(-1)
 using namespace std;
 
 
 int main()
 {
-    int t,Case;
-    sf(t);
+    int t;
+    cin>>t;
 
-    for(Case=1; Case<=t; Case++)
+    for(int Case=1; Case<=t; Case++)
     {
-        ll n,x,y,cnt=1,num=1,p=1;
-        sfl(n);
+        ll n;
+        cin>>n;
 
-        while(num<=n)
+        cout<<"Case "<<Case<<": ";
+
+        if(n==1)
         {
-            if(p%2=0)
-            {
-                p++;
-                num+=1;
-                cnt++;
 
+            cout<<"1 1"<<nl;
+            continue;
+        }
+
+        ll sq=ceil(sqrt(n));
+
+        sq--;
+
+        ll diagonal=2*((sq*(sq+1))/2)+1;
+
+         ll x=sq+1,y=sq+1;
+
+        if(sq%2!=0)
+        {
+            if(diagonal-n>=0)
+            {
+
+                x-=(diagonal-n);
             }
             else
             {
-                cnt+=cnt+2;
-                num+=cnt;
-                p++;
+
+                y-=(n-diagonal);
             }
+
+        }
+        else
+        {
+             if(diagonal-n>=0)
+            {
+
+                y-=(diagonal-n);
+            }
+            else
+            {
+
+                x-=(n-diagonal);
+            }
+
         }
 
+        cout<<x<<" "<<y<<nl;
 
     }
 
+
     return 0;
 }
+
+
 
